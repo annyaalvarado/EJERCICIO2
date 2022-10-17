5b2a66fb31da6383252454d8f90396ba5f05c35b
Jonatan <Jonatan@ANNYALVARADO>
2022-10-17 09:59:40 -0600

    Agregar archivos de proyecto.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using static Xamarin.Forms.Internals.Profile;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var datos = new Models.Datos { nombre = txtnombre.Text, apellido = txtapellido.Text, edad = Convert.ToInt32(txtedad.Text), email = txtcorreo.Text };
+            var Page2 = new Views.Page2();
+            Page2.BindingContext = datos;
+
+            await Navigation.PushAsync(Page2);
+        }
+
+
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
---
+++ App1/App1/Models/Datos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Models
+{
+    public class Datos
+    {
+        public String nombre { get; set; }
+        public String apellido { get; set; }
+        public int edad { get; set; }
+        public String email { get; set; }
+    }
+}
